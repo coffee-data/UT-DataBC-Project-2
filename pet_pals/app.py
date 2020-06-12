479e2532b7a722a70c1e6a3c35b88d13cf23f80d
coffee-data <jesse.ryan.hartman@gmail.com>
2020-06-11 19:38:08 -0500

    try again, not saved?
    try last commit again

--- pet_pals/app.py
+++ pet_pals/app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/form")
+@app.route("/viz-1")
 def embed():
-    return render_template("form.html")
+    return render_template("viz-1.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
